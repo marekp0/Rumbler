f0eec53eff45877433efbcd68ad1fb7fd7678965
Marek Pokorny <marekp0gh@gmail.com>
2023-12-20 00:28:03 -0800

    Move SliderHapticFeedbackInteractionEffect to use CustomHapticFeedbackPlayer

--- Rumbler/HarmonyPatches/SliderHapticFeedbackInteractionEffect.cs
+++ Rumbler/HarmonyPatches/SliderHapticFeedbackInteractionEffect.cs
@@ -2,16 +2,24 @@
 using IPA.Utilities;
 using Libraries.HM.HMLib.VR;
 using Rumbler.Configuration;
+using UnityEngine;
 
 namespace Rumbler.HarmonyPatches
 {
-    [HarmonyPatch(typeof(SliderHapticFeedbackInteractionEffect), "Start")]
-    internal class SliderHapticFeedbackInteractionEffect_Start
+    [HarmonyPatch(typeof(SliderHapticFeedbackInteractionEffect), "Vibrate")]
+    internal class SliderHapticFeedbackInteractionEffect_Vibrate
     {
-        private static void Postfix(SliderHapticFeedbackInteractionEffect __instance)
+        private static bool Prefix(SliderHapticFeedbackInteractionEffect __instance)
         {
-            var hapticPreset = __instance.GetField<HapticPresetSO, SliderHapticFeedbackInteractionEffect>("_hapticPreset");
-            //PluginConfig.Instance.Slider.CopyTo(hapticPreset);
+            // figure out rumble info
+            var rumbleInfo = PluginConfig.Instance.Slider.ToRumbleInfo();
+            rumbleInfo.rumbleDuration = Time.deltaTime;
+
+            var saberType = __instance.GetField<SaberType, SliderHapticFeedbackInteractionEffect>("_saberType");
+            var player = RumblerController.Instance?.HapticFeedbackPlayer;
+            player?.PlayHapticFeedback(saberType.Node(), rumbleInfo);
+
+            return false;
         }
     }
 }
